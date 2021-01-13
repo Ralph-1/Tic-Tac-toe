b9ef4b6e1295aadff8337af9f6d2acb428a9afd5
Ralph <ralphoburu@gmail.com>
2021-01-13 18:28:56 +0300

    delete 'main' file from bin

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,3 @@
-# #!/usr/bin/env ruby
+#!/usr/bin/env ruby
 
-# puts 'Hello World'
-
-# def instructions
-#   system('clear')
-#   puts "You will have a 3x3 board and player 1 will have the first turn\n"
-#   puts "When it's your turn enter the number of the square that you want to play (between 1 and 9)"
-#   puts "Don't choose squares that are already occupied\n\n"
-#   puts '    -------------'
-#   puts '    | 1 | 2 | 3 |'
-#   puts '    -------------'
-#   puts '    | 4 | 5 | 6 |'
-#   puts '    -------------'
-#   puts '    | 7 | 8 | 9 |'
-#   puts '    -------------'
-#   puts "\nThe winner is the first one to form a line with 3 symbols vertically, horizontally or diagonally\n\n"
-#   puts 'Press Enter to continue'
-#   gets.chomp
-# end
-
-# puts in
+puts 'Hello World'
