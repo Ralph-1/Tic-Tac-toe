b124e317cf5c87dfdd16c4dec26cdebd6ee3327e
Ralph <ralphoburu@gmail.com>
2021-01-20 15:11:09 +0300

    add the all case for the game to end

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,5 +1,7 @@
 # Method to convert users input to match the position on the board
-class Helpers
+class 
+  winning_cases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
+
   def position_taken?(board, index) 
     if board[index] == (board[index] == '') || (board[index] == ' ') || (board[index] == nil)
       false
@@ -12,4 +14,16 @@ class Helpers
     index.between?(0, 8) && !position_taken?(board, index)
   end
 
+  def won(board)
+    winning_cases.each do |case|
+      board[case[0]] == board[case[1] && board[case[1]] == board[case[2]] && board[case[0]] != ' '
+  end
+
+  def draw?(turn)
+    turn >= 9
+  end
+
+  def over?(board, turn)
+    won?(board) || draw?(board)
+  end
 end
